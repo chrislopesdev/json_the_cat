cd4ba5fff7a35aa40f463d72b90076994826f5e1
chris lopes <chris.lopes@gmail.com>
2021-12-03 00:32:26 +0000

    add test mocha chai

--- README.md
+++ README.md
@@ -7,3 +7,18 @@
 `npm install request`
 
 `touch .gitignore && echo "node_modules/" >> .gitignore`
+
+# Testing
+
+`npm install mocha chai --save-dev`
+
+Create a test directory and test file.
+
+Update JSON file to use mocha for the test
+```
+  "scripts": {
+    "test": "mocha"
+  }
+```
+
+Run test using  `npx mocha`
--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,22 @@
 // require request node
 const request = require('request');
-
 // define function w two parameters breed, and anonymous callback (see index.js)
 const fetchBreedDescription = (breed, callback) => {
   // breed gets passed into api url
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-
   // make data request from api
   request(url, (error, response, body) => {
-
     // if error, call callback (index.js)
     if (error) {
       callback(error, null);
       // exit the function if there's an error
       return;
     }
-
     // console.log(typeof body); //=> string
     // convert string to object we can use
     const data = JSON.parse(body);
-
-    // console.log(data);
+    console.log(data);
     // console.log(typeof data); //=> object
-  
     // if data is empty => log breed not found
     if (data.length === 0) {
       // console.log(`Breed Not Found`);
@@ -30,12 +24,11 @@ const fetchBreedDescription = (breed, callback) => {
       callback('Breed not found!');
     // else give breed description
     } else {
-      // console.log(data[0].description);
+      console.log(data[0].description);
       // no more log => use callback
       // use null to omit error parameter and log description
       callback(null, data[0].description);
     }
-  
   });
 };
 
